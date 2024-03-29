cb4fc8475ad6e94bde44e6aa84ec7cf340a3b9a4
Lev Saunders <levsau00@gmail.com>
2024-03-29 19:34:01 -0400

    add sample run script found online

--- ml/fomo/run_eim_example.py
+++ ml/fomo/run_eim_example.py
@@ -1,192 +1,116 @@
-#!/usr/bin/env python
-
-# import device_patches
-# Device specific patches for Jetson Nano (needs to be before importing cv2)
+# FROM https://colab.research.google.com/github/edgeimpulse/notebooks/blob/main/notebooks/object-counting-using-fomo.ipynb#scrollTo=57dPBIW6Wwsv
 
 import cv2
 import os
-import sys, getopt
-import signal
 import time
+import sys, getopt
+import numpy as np
 from edge_impulse_linux.image import ImageImpulseRunner
 
 
+modelfile = '/home/raspi/Autonomous_Cat_Deterrence/ml/fomo/models/catfomo2-linux-aarch64-v3.eim'
+videofile = '/home/raspi/Autonomous_Cat_Deterrence/data/benchmark_vids/cat_counter5.mp4'
+
+
 runner = None
-# if you don't want to see a camera preview, set this to False
+# if you don't want to see a video preview, set this to False
 show_camera = True
-if sys.platform == "linux" and not os.environ.get("DISPLAY"):
+if (sys.platform == 'linux' and not os.environ.get('DISPLAY')):
     show_camera = False
+print('MODEL: ' + modelfile)
 
-
-def now():
-    return round(time.time() * 1000)
-
-
-def get_webcams():
-    port_ids = []
-    for port in range(5):
-        print("Looking for a camera in port %s:" % port)
-        camera = cv2.VideoCapture(port)
-        if camera.isOpened():
-            ret = camera.read()[0]
-            if ret:
-                backendName = camera.getBackendName()
-                w = camera.get(3)
-                h = camera.get(4)
-                print(
-                    "Camera %s (%s x %s) found in port %s " % (backendName, h, w, port)
-                )
-                port_ids.append(port)
-            camera.release()
-    return port_ids
-
-
-def sigint_handler(sig, frame):
-    print("Interrupted")
-    if runner:
-        runner.stop()
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, sigint_handler)
-
-
-def help():
-    print(
-        "python classify.py <path_to_model.eim> <Camera port ID, only required when more than 1 camera is present>"
-    )
-
-
-def main(argv):
+with ImageImpulseRunner(modelfile) as runner:
     try:
-        opts, args = getopt.getopt(argv, "h", ["--help"])
-    except getopt.GetoptError:
-        help()
-        sys.exit(2)
+        model_info = runner.init()
+        print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
+        labels = model_info['model_parameters']['labels']
+        count = 0
+        vidcap = cv2.VideoCapture(videofile)
+        sec = 0
+        start_time = time.time()
 
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            help()
-            sys.exit()
-
-    if len(args) == 0:
-        help()
-        sys.exit(2)
-
-    model = args[0]
-
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    modelfile = os.path.join(dir_path, model)
-
-    print("MODEL: " + modelfile)
-
-    with ImageImpulseRunner(modelfile) as runner:
-        try:
-            model_info = runner.init()
-            print(
-                'Loaded runner for "'
-                + model_info["project"]["owner"]
-                + " / "
-                + model_info["project"]["name"]
-                + '"'
-            )
-            labels = model_info["model_parameters"]["labels"]
-            if len(args) >= 2:
-                videoCaptureDeviceId = int(args[1])
+        def getFrame(sec):
+            vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+            hasFrames,image = vidcap.read()
+            if hasFrames:
+                return image
             else:
-                port_ids = get_webcams()
-                if len(port_ids) == 0:
-                    raise Exception("Cannot find any webcams")
-                if len(args) <= 1 and len(port_ids) > 1:
-                    raise Exception(
-                        "Multiple cameras found. Add the camera port ID as a second argument to use to this script"
-                    )
-                videoCaptureDeviceId = int(port_ids[0])
-
-            camera = cv2.VideoCapture(videoCaptureDeviceId)
-            ret = camera.read()[0]
-            if ret:
-                backendName = camera.getBackendName()
-                w = camera.get(3)
-                h = camera.get(4)
-                print(
-                    "Camera %s (%s x %s) in port %s selected."
-                    % (backendName, h, w, videoCaptureDeviceId)
-                )
-                camera.release()
-            else:
-                raise Exception("Couldn't initialize selected camera.")
-
-            next_frame = 0  # limit to ~10 fps here
-
-            for res, img in runner.classifier(videoCaptureDeviceId):
-                if next_frame > now():
-                    time.sleep((next_frame - now()) / 1000)
-
-                # print('classification runner response', res)
-
-                if "classification" in res["result"].keys():
-                    print(
-                        "Result (%d ms.) "
-                        % (res["timing"]["dsp"] + res["timing"]["classification"]),
-                        end="",
-                    )
-                    for label in labels:
-                        score = res["result"]["classification"][label]
-                        print("%s: %.2f\t" % (label, score), end="")
-                    print("", flush=True)
-
-                elif "bounding_boxes" in res["result"].keys():
-                    print(
-                        "Found %d bounding boxes (%d ms.)"
-                        % (
-                            len(res["result"]["bounding_boxes"]),
-                            res["timing"]["dsp"] + res["timing"]["classification"],
-                        )
-                    )
-                    prices = {
-                        "cadbury_DM": 1.1,
-                        "indomie_goreng": 0.4,
-                        "kitkat": 0.6,
-                        "kitkat_gold": 0.8,
-                        "mentos": 0.7,
-                        "milo_nuggets": 1.0,
-                        "pocky_chocolate": 1.2,
-                        "toblerone": 2.0,
-                    }
-                    # set item price
-                    total = 0
-                    for bb in res["result"]["bounding_boxes"]:
-                        print(
-                            "\t%s (%.2f): x=%d y=%d w=%d h=%d"
-                            % (
-                                bb["label"],
-                                bb["value"],
-                                bb["x"],
-                                bb["y"],
-                                bb["width"],
-                                bb["height"],
-                            )
-                        )
-                        img = cv2.rectangle(
-                            img,
-                            (bb["x"], bb["y"]),
-                            (bb["x"] + bb["width"], bb["y"] + bb["height"]),
-                            (255, 0, 0),
-                            1,
-                        )
-                        total += prices[bb["label"]]  # set total price
-
-                if show_camera:
-                    cv2.imshow("edgeimpulse", cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
-                    if cv2.waitKey(1) == ord("q"):
-                        break
-
-                next_frame = now() + 100
-        finally:
-            if runner:
-                runner.stop()
+                print('Failed to load frame', videofile)
+                exit(1)
 
 
-if __name__ == "__main__":
-    main(sys.argv[1:])
+        img = getFrame(sec)
+        
+    
+        # Define the top of the image and the number of columns
+        TOP_Y = 30
+        NUM_COLS = 5
+        COL_WIDTH = int(vidcap.get(3) / NUM_COLS)
+        # Define the factor of the width/height which determines the threshold
+        # for detection of the object's movement between frames:
+        DETECT_FACTOR = 1.5
+
+        # Initialize variables
+        count = [0] * NUM_COLS
+        countsum = 0
+        previous_blobs = [[] for _ in range(NUM_COLS)]
+        
+        while img.size != 0:
+            # imread returns images in BGR format, so we need to convert to RGB
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = cv2.resize(img, (96,96))
+
+            # get_features_from_image also takes a crop direction arguments in case you don't have square images
+            features, cropped = runner.get_features_from_image(img)
+            img2 = cropped
+            COL_WIDTH = int(np.shape(cropped)[0]/NUM_COLS)
+
+            # the image will be resized and cropped, save a copy of the picture here
+            # so you can see what's being passed into the classifier
+            cv2.imwrite('debug.jpg', cv2.cvtColor(cropped, cv2.COLOR_RGB2BGR))
+
+            res = runner.classify(features)
+            # Initialize list of current blobs
+            current_blobs = [[] for _ in range(NUM_COLS)]
+            
+            if "bounding_boxes" in res["result"].keys():
+                print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
+                for bb in res["result"]["bounding_boxes"]:
+                    print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
+                    img2 = cv2.rectangle(cropped, (bb['x'], bb['y']), (bb['x'] + bb['width'], bb['y'] + bb['height']), (255, 0, 0), 1)
+
+                        # Check which column the blob is in
+                    col = int(bb['x'] / COL_WIDTH)
+
+                    # Check if blob is within DETECT_FACTOR*h of a blob detected in the previous frame and treat as the same object
+                    for blob in previous_blobs[col]:
+                        if abs(bb['x'] - blob[0]) < DETECT_FACTOR * (bb['width'] + blob[2]) and abs(bb['y'] - blob[1]) < DETECT_FACTOR * (bb['height'] + blob[3]):
+                        # Check this blob has "moved" across the Y threshold
+                            if blob[1] >= TOP_Y and bb['y'] < TOP_Y:
+                                # Increment count for this column if blob has left the top of the image
+                                count[col] += 1
+                                countsum += 1
+                    # Add current blob to list
+                    current_blobs[col].append((bb['x'], bb['y'], bb['width'], bb['height']))
+
+
+
+            # Update previous blobs
+            previous_blobs = current_blobs
+
+            if (show_camera):
+                im2 = cv2.resize(img2, dsize=(800,800))
+                cv2.putText(im2, f'{countsum} items passed', (15,750), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+                cv2.imshow('edgeimpulse', cv2.cvtColor(im2, cv2.COLOR_RGB2BGR))
+                print(f'{count}')
+                if cv2.waitKey(1) == ord('q'):
+                    break
+
+            sec = time.time() - start_time
+            sec = round(sec, 2)
+            # print("Getting frame at: %.2f sec" % sec)
+            img = getFrame(sec)
+    finally:
+        if (runner):
+            print(f'{countsum} Items Left Conveyorbelt')
+            runner.stop()
