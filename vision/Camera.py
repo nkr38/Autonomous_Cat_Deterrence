fde26e2102fb462fb40662d83ade956ad698161f
Lev Saunders <levsau00@gmail.com>
2024-05-19 20:18:57 -0400

    update Camera class

--- vision/Camera.py
+++ vision/Camera.py
@@ -18,12 +18,14 @@ class Camera:
         self.encoder = H264Encoder()
 
         now = datetime.datetime.now().strftime("%H-%M-%S_%d-%m-%Y")
-        if Path("/media/raspi/catdrive/").is_dir():
-            self.output = FfmpegOutput(f"/media/raspi/catdrive/vid_{now}.mp4")
-        else:
-            self.output = FfmpegOutput(f"vid_{now}.mp4")
+        # if Path("/media/raspi/catdrive/").is_dir():
+        #     self.output = FfmpegOutput(f"/media/raspi/catdrive/vid_{now}.mp4")
+        # else:
+        self.output = FfmpegOutput(f"vid_{now}.mp4")
         
         self.fov = (102,67)
+        self.fov = (90,67)
+        self.fov = (80,67)
 
         self.main_size = main_size
         self.lores_size = lores_size
@@ -37,6 +39,7 @@ class Camera:
         self.model_input_size = model_input_size
         
         self.closed = True
+        self.recording = False
     
     def __enter__(self):
         if self.picam2 is None:
@@ -44,22 +47,30 @@ class Camera:
         self.closed = False
         return self
     def __exit__(self, type, value, traceback):
+        if self.recording:
+            self.stop_recording()
         self.picam2.stop()
         self.closed = True
 
     def start_recording(self):
         self.picam2.start_recording(self.encoder, self.output)
+        self.recording = True
 
     def stop_recording(self):
         self.picam2.stop_recording()
+        self.recording = False
 
     def greyscale(self, img: np.ndarray)-> np.ndarray: 
         return np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])
 
     def capture_main(self, greyscale: bool = False) -> np.ndarray:
+        if not self.recording:
+            self.start_recording()
         cap = self.picam2.capture_array("main")
         return cap if not greyscale else self.greyscale(cap)
 
     def capture_lores(self, greyscale: bool = False) -> np.ndarray:
+        if not self.recording:
+            self.start_recording()
         cap = self.picam2.capture_array("lores")
         return cap if not greyscale else self.greyscale(cap)
